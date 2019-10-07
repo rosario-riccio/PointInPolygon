229f9e3c9fc5f5866f52ed031eb7fa64e056e6c7
rosario riccio <32274470+rosario-riccio@users.noreply.github.com>
2019-10-08 00:44:51 +0200

    Update myFunctions.py

--- myFunctions.py
+++ myFunctions.py
@@ -3,6 +3,8 @@ A) to check if there are polygon without label
 B) to seek file netCDF in local storage/internet of specific date"""
 from shapely.geometry import Point, Polygon
 from dbMongo import *
+from datetime import date, timedelta,datetime
+
 from sys import exit
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon as po
@@ -15,20 +17,22 @@ import urlparse
 import os
 import sys
 import wget
-import datetime
+
 
 #insert your path
-localPath = "<insert your path where are netcdf files>"
-
+localPath = "/home/rosario/Scrivania/dataTemp1"
+#localPath = "/Users/rosarioriccio/Desktop/dataTemp"
 url = "http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5"
 
 
 """Step 1:
-A) to check if DB is actived
-B) to check if labels exists
-C) to check if polygons exists
-D) to check if every polygon has its label
+A) to check if DB was actived
+B) to check if labels existed
+C) to check if polygons existed
+D) to check if every polygon had its label
 """
+
+#Controllo attivazione DB,presenza label,polygon,polygon senza etichetta
 try:
     managedb1 = ManageDB()
 except Exception as e:
@@ -65,56 +69,45 @@ class MyTool(object):
         """This is constructor"""
         pass
 
-    def getUrls(self,rStart,rEnd,yStart,yEnd,mStart,mEnd,dStart,dEnd,hStart,hEnd):
-        """this method allows to create a specific path and seeks this one on local storage or internet"""
+
+    def getUrls1(self,rStart,rEnd,yStart,mStart,dStart,hStart,yEnd,mEnd,dEnd,hEnd):
         urls = []
         ncfiles = []
-        print("-------------")
-        for r in range(rStart, rEnd+1):  # resolution
+        for r in range(rStart,rEnd+1):
             r1 = "d0" + str(r)
-            # print(r1)
-            for y in range(yStart, yEnd+1):  # year
-                for m in range(mStart, mEnd+1):  # month
-                    if m < 10:
-                        m1 = "0" + str(m)
-                    else:
-                        m1 = str(m)
-                    for d in range(dStart, dEnd+1):  # day
-                        if d < 10:
-                            d1 = "0" + str(d)
-                        else:
-                            d1 = str(d)
-                        for h in range(hStart, hEnd+1):  # hour
-                            if h < 10:
-                                h1 = "0" + str(h)
-                            else:
-                                h1 = str(h)
-                            url1 = url + "/" + r1 + "/" + "archive" + "/" + str(
-                                y) + "/" + m1 + "/" + d1 + "/" + "wrf5_" + r1 + "_" + str(
-                                y) + m1 + d1 + "Z" + h1 + "00" + ".nc"
-                            fname = "wrf5_" + r1 + "_" + str(y) + m1 + d1 + "Z" + h1 + "00" + ".nc"
-                            print(url1)
-                            # http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5/d01/archive/2018/08/01/wrf5_d01_20180801Z0000.nc
-                            print(fname)
-                            localPath1 = localPath + "/" + r1
-                            localPath2 = localPath1 + "/" + fname
-                            print(localPath2)
-                            if not os.path.isfile(localPath2):
-                                print("file doesn't exist in local storage: " + fname)
-                                try:
-                                    nc = Dataset(url1)
-                                    nc.close()
-                                    urls.append(url1)
-                                    # filename = wget.download(url1,"/Users/rosarioriccio/PycharmProjects/prova1/d01")
-                                    # print(filename)
-                                    # print("file saved in local storage: " + fname)
-                                    print("file exists online: " + fname)
-                                except Exception as e:
-                                    print("file doesn't exists online")
-                            else:
-                                print("file exists in local storage: " + fname)
-                                ncfiles.append(localPath2)
-        return ncfiles,urls
+            sdate = datetime(yStart,mStart,dStart,hStart)
+            edate = datetime(yEnd,mEnd,dEnd,hEnd)
+            delta = int((edate-sdate).total_seconds()/3600)
+            for i in range(delta+1):
+                tempDate = sdate + timedelta(hours=i)
+                time1 = tempDate.strftime("%Y%m%dZ%H%M")
+                year = tempDate.strftime("%Y")
+                month = tempDate.strftime("%m")
+                day = tempDate.strftime("%d")
+                url1 = url + "/" + r1 + "/" + "archive" + "/" + year + "/" + month + "/" + day + "/" + "wrf5_" + r1 + "_" + time1 + ".nc"
+                fname = time1 + ".nc"
+                print(url1)
+                print(fname)
+                # http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5/d01/archive/2018/08/01/wrf5_d01_20180801Z0000.nc
+                localPath1 = localPath + "/" + r1
+                localPath2 = localPath1 + "/" + fname
+                print(localPath2)
+                if not os.path.isfile(localPath2):
+                    print("file non presente in locale: " + fname)
+                    try:
+                        nc = Dataset(url1)
+                        nc.close()
+                        urls.append(url1)
+                        # filename = wget.download(url1,"/Users/rosarioriccio/PycharmProjects/prova1/d01")
+                        # print(filename)
+                        # print("file salvato in locale: " + fname)
+                        print("file presente online: " + fname)
+                    except Exception as e:
+                        print("file non presente online")
+                else:
+                    print("file presente in locale: " + fname)
+                    ncfiles.append(localPath2)
+        return urls,ncfiles
 
     def getPolygon(self,date1,managedb):
         """this method gets the polygons of specific date"""
@@ -146,7 +139,6 @@ class MyTool(object):
             return None,None
 
     def assignTypeStorm(self,managedb,polygon):
-        """This method associates a specific value to label"""
         result,cursorLabelCollection1 = managedb.listLabelCollectionDB()
         if(result != True):
            print("Error: there aren't labels")
@@ -160,6 +152,5 @@ class MyTool(object):
                 labelId = label["labelId"]
                 #print("type",labelId)
                 return labelId
-
 
 
